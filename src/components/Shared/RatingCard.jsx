0b8d21a6bb6b361e970c455fd643388983e6e563
Ahmed Karim <ahmed.karim.lahmar@gmail.com>
2025-02-25 16:52:22 +0100

    feat: add testimonials (wip)

--- src/components/Shared/RatingCard.jsx
+++ src/components/Shared/RatingCard.jsx
@@ -1,14 +1,18 @@
+import styles from "./RatingCard.module.css";
+import placeholder from "@assets/placeholder.svg";
+import star from "@assets/star.svg";
+
 const RatingCard = ({ className, username, review, rating, children }) => {
   return (
-    <article>
-      <img src="" alt="User Picture" />
+    <article className={`${styles.card} ${className}`}>
+      <img className={styles.userpic} src={placeholder} alt="User Picture" />
       <div>
         <h3>{username}</h3>
         <p>{review}</p>
-        <div className="stars">
-          <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" />
+        <div className={styles.stars}>
+          {Array.from({ length: rating }).map((_, index) => (
+            <img key={index} src={star} alt="" />
+          ))}
         </div>
       </div>
     </article>
--- src/components/Testimonials/TestimonialsComp.jsx
+++ src/components/Testimonials/TestimonialsComp.jsx
@@ -1,13 +1,23 @@
 import { RatingCard } from "@components/Shared";
+import styles from "./TestimonialsComp.module.css";
 
+const cardTemplate = {
+  username: "testuser",
+  review: "lorem ipsum",
+  rating: 3,
+};
 const Testimonials = () => {
+  const cards = Array.from({ length: 3 }).map((_, index) => ({
+    ...cardTemplate,
+  }));
   return (
-    <section>
-      <h2>Testimonials</h2>
-      <RatingCard username={"testuser"} review={"lorem ipsum"} rating={3} />
-      <RatingCard username={"testuser"} review={"lorem ipsum"} rating={3} />
-      <RatingCard username={"testuser"} review={"lorem ipsum"} rating={3} />
-      <RatingCard username={"testuser"} review={"lorem ipsum"} rating={3} />
+    <section className={styles.testimonials}>
+      <h2 className={`${styles.title} sub-title`}>Testimonials</h2>
+      <div className={styles.cards}>
+        {cards.map((card, index) => (
+          <RatingCard key={index} {...card} className={styles.card} />
+        ))}
+      </div>
     </section>
   );
 };
